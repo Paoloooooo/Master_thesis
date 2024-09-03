20193ba3668cf7b1318e97e7d50c32ea1e32ef94
Paoloooooo <paolo.franco2000@gmail.com>
2024-09-03 13:18:07 +0200

    Fixed missing files after installation

--- installation/install_ari_sim.sh
+++ installation/install_ari_sim.sh
@@ -3,7 +3,7 @@ mkdir ./ari_ws
 cd ./ari_ws
 
 # Installing ari dependencies
-sudo apt install python-is-python3
+sudo apt install python-is-python3 # Required since in some roslaunch files it uses python instead of python3
 curl https://raw.githubusercontent.com/pal-robotics/ari_tutorials/master/ari_public-noetic.rosinstall > ari_dependencies.txt
 rosinstall src /opt/ros/noetic ari_dependencies.txt
 rm ari_dependencies.txt
@@ -17,3 +17,15 @@ rosdep install --from-paths src --ignore-src --rosdistro noetic --skip-keys="ope
 # Building the workspace
 source /opt/ros/noetic/setup.bash
 catkin build -DCATKIN_ENABLE_TESTING=0
+
+# Copying some files to fix simulation bugs
+#   these files seems to be used by launchers with different names 
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/state_machine/state_machine_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/state_machine/state_machine_public_sim_laser.yaml
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_plugins_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_plugins_public_sim_laser.yaml
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_plugins_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_plugins_public_sim_laser.yaml
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/global_costmap_public_sim_laser.yaml
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/local_costmap_plugins_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/local_costmap_plugins_public_sim_laser.yaml
+cp src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/local_costmap_public_sim.yaml src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/base/common/local_costmap_public_sim_laser.yaml
+
+curl https://raw.githubusercontent.com/Robocup-Lyontech/Palbator_simulation/3459b17f47ad91d4a08fb680d3af2b1cf4b14864/pal_navigation_cfg_public/pal_navigation_cfg_ari/launch/mapping_slam_toolbox.launch > src/pal_navigation_cfg_public/pal_navigation_cfg_ari/launch/mapping_slam_toolbox.launch
+curl https://raw.githubusercontent.com/Robocup-Lyontech/Palbator_simulation/3459b17f47ad91d4a08fb680d3af2b1cf4b14864/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/mapping/slam_toolbox.yaml > src/pal_navigation_cfg_public/pal_navigation_cfg_ari/config/mapping/slam_toolbox.yaml
